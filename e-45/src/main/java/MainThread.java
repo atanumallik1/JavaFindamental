67f4014bde6c13320df2ae97ca2dc34b55d31bde
“Atanu <atanu.mallik@sap.com>
2021-11-11 23:19:59 +0530

    ss

---
+++ e-45/src/main/java/MainThread.java
@@ -0,0 +1,15 @@
+
+public class MainThread {
+	public static void main(String args[]) {
+		
+		
+		MyThread th = new MyThread();
+		//th.start();
+		th.run();
+		
+		System.out.println("End !" +Thread.currentThread().getName());
+		
+
+	}
+
+}
